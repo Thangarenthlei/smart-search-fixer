c6b1eefcbb0b304e24c8df8ddc9677e324108199
Thangarenthlei <cthangarenthlei@gmail.com>
2025-12-28 20:52:06 +0530

    Add Shopify Node adapter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { shopifyApi } from "@shopify/shopify-api";
+import "@shopify/shopify-api/adapters/node";
 
 dotenv.config();
 
